415de6ea7e9dff2157cbcba195fbd09f0a713561
Roberstxx07 <132015061+Roberstxx07@users.noreply.github.com>
2023-10-06 08:59:43 -0600

    Create BatallaPokemon.java

---
+++ BatallaPokemon.java
@@ -0,0 +1,21 @@
+public class BatallaPokemon {
+    
+     public static void main(String[] args) {
+        // Ejemplo de uso
+         pokemos_Fuego = new pokemos_Fuego ("Fuego", "Planta/Veneno", 15, 50);
+
+        System.out.println("Estado de Fuego:");
+        System.out.printf("Nombre: %s\n", Fuego.getNombre());
+        System.out.printf("Tipo: %s\n", Fuego.getTipo());
+        System.out.printf("Nivel: %d\n", Fuego.getNivel());
+        System.out.printf("HP: %d\n", Fuego.getHP());
+        System.out.printf("Experiencia: %d\n", Fuego.getExperiencia());
+
+        Fuego.atacar("Látigo Cepa", new Pokemon("Rattata", "Normal", 10));
+        Fuego.ganarExperiencia(25);
+
+        System.out.println("Estado de Fuego después de un ataque y ganar experiencia:");
+        System.out.printf("HP: %d\n", Fuego.getHP());
+        System.out.printf("Experiencia: %d\n", Fuego.getExperiencia());
+    }
+}
