0f269bb47102a246bdf50ab3ef4c8561ee90f5fe
Roberstxx07 <132015061+Roberstxx07@users.noreply.github.com>
2023-10-12 08:54:01 -0600

    Update and rename PokemonFuego to PokemonFuego.java

---
+++ PokemonFuego.java
@@ -0,0 +1,6 @@
+public class PokemonFuego extends Pokemon {
+    public PokemonFuego(String nombre, int nivel) {
+        super(nombre, "Fuego", nivel); // Corregido: tipo "Fuego" en lugar de "Agua"
+    }
+    
+}
